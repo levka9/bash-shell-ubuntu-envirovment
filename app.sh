6074659efb1323bcea718e0f478a13da89b09c05
levka9 <levkir@gmail.com>
2022-07-05 12:59:32 +0300

    fix array string in the for loop

--- app.sh
+++ app.sh
@@ -18,7 +18,7 @@ SNAP_PACKAGES=("chromium" "--classic code" "vlc" "whatsapp-app" "telegram-deskto
 # sudo add-apt-repository -y ppa:hluk/copyq #copyq
 
 echo "starting to update packages ..."
-for pkg in ${ADD_APT_REPOSITORY[@]};
+for pkg in "${ADD_APT_REPOSITORY[@]}";
 do
     echo "-- install '$pkg'"
     sudo add-apt-repository -y "$pkg" &> /dev/null
@@ -27,12 +27,12 @@ done
 echo "finished the packages updating!"
 
 echo "starting to install apt-get applications …"
-for pkg in ${APT_GET_PACKAGES[@]};
+for pkg in "${APT_GET_PACKAGES[@]}";
 do
     echo "-- install '$pkg'"
     is_package_exists="$(is_package_exists "$pkg")"
         
-    if [[ "$is_package_exists" == "1" ]]; then
+    if [[ "$is_package_exists" == "0" ]]; then
         echo "Package $pkg already installed."
     else
         sudo apt-get install -y $pkg
@@ -41,19 +41,19 @@ done
 echo "finished to install apt-get applications!"
 
 echo "starting to install snap applications …"
-for pkg in ${SNAP_PACKAGES[@]};
+for pkg in "${SNAP_PACKAGES[@]}";
 do
     echo "-- install '$pkg'"
     is_package_exists "$pkg"
-    is_package_exists_ret=$ret_value
+    is_package_exists_ret="$ret_value"
     
     #echo "is_package_exists_ret="$is_package_exists_ret #debugger 
 
-    if [[ $is_package_exists_ret==0 ]]; then
+    if [[ "$is_package_exists_ret" == "0" ]]; then
         echo "Package $pkg already installed."
     else
         echo "start install snap"
-        sudo snap install $pkg
+        sudo snap install "$pkg"
     fi
 done
 echo "finished to install snap applications!"
